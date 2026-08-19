f7339797df1e7fe38f2656025d83f1a8d9a7ee20
Rajapandi29 <rp6321613@gmail.com>
2026-08-19 22:05:08 +0530

    new

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "AWS region in which to run the application."
   type        = string
-  default     = "ap-south-1"
+  default     = "us-east-1"
 }
 
 variable "app_repository" {
